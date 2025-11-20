392f9056249b159756be0f67c73a874f84f6b7c3
pvunderink <pepijn@vund.nl>
2025-11-20 16:11:48 +0100

    Improved default Flow implementation

--- src/flow.rs
+++ src/flow.rs
@@ -4,14 +4,4 @@ use num::Zero;
 
 pub trait Flow: Copy + PartialOrd + Add<Output = Self> + Sub<Output = Self> + Zero {}
 
-macro_rules! impl_flow {
-    ($($t:ty),*) => {
-        $(
-            impl Flow for $t {}
-        )*
-    };
-}
-
-impl_flow!(
-    i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64
-);
+impl<T> Flow for T where T: Copy + PartialOrd + Add<Output = T> + Sub<Output = T> + Zero {}
